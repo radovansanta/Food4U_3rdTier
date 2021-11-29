3fe04d81a05b64512a16f01804bcc18792402de1
biancamilitaru <75315082+biancamilitaru@users.noreply.github.com>
2021-11-29 13:55:11 +0100

    Created: - Category manager

---
+++ src/Managers/CategoryManager.java
@@ -0,0 +1,23 @@
+package Managers;
+
+import DatabaseAccess.Food4UDAO;
+import Models.Category;
+import Models.Restaurant;
+import com.google.gson.Gson;
+import com.google.gson.JsonSyntaxException;
+
+public class CategoryManager {
+    Food4UDAO food4UDAO = Food4UDAO.getInstance();
+    Gson gson = new Gson();
+    Restaurant restaurant;
+
+    public void addCategory(String categoryAsJson){
+        System.out.println(categoryAsJson);
+        try{
+            Category category = gson.fromJson(categoryAsJson, Category.class);
+            food4UDAO.addCategory(category, restaurant.getRestaurantID());
+        } catch (JsonSyntaxException e) {
+            e.printStackTrace();
+        }
+    }
+}
