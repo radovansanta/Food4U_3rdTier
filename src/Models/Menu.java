92dc6c2398e62063eba745ba26da05aeb42b763f
Tolnai <tolnai.milan22@gmail.com>
2021-11-29 11:25:08 +0100

    Menu model

---
+++ src/Models/Menu.java
@@ -0,0 +1,39 @@
+package Models;
+
+public class Menu
+{
+  private int menuID;
+  private String description;
+
+  public Menu (int menuID, String description)
+  {
+    this.description= description;
+    this.menuID = menuID;
+  }
+
+
+  public int getMenuID()
+  {
+    return menuID;
+  }
+
+  public String getDescription()
+  {
+    return description;
+  }
+
+  public void setMenuID(int menuID)
+  {
+    this.menuID = menuID;
+  }
+
+  public void setDescription(String description)
+  {
+    this.description = description;
+  }
+
+  public String toString()
+  {
+    return "Menu{" + "menuID= "+menuID+", description= "+description+"}";
+  }
+}
